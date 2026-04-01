354a24422403f69e209bb29e2e436a606c9389c1
Francesco Floris <francescofloris.ef@gmail.com>
2026-04-01 16:10:58 +0200

    funzione 2 stampa solo iniziali

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,13 +1,20 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-
+let namesFirst = [];
 
 // Dichiara la funzione qui.
+
+function inizialiNomi (namesFirst) {
+    for (let i = 0; i < names.length; i++) {
+        namesFirst[i] = names[i][0];
+    }
+    return namesFirst;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(inizialiNomi(namesFirst));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
